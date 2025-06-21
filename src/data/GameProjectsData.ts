caffde1ee457a31be9d616755ee42710258abbe8
anis2720 <138358919+anis2720@users.noreply.github.com>
2025-06-21 14:30:04 +0800

    Update build

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -1,153 +1,157 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("project-1", "Optimistic Chubby", "img/projects/project-1-icon.png", 
+
+    //Stranded
+    new ProjectData("project-1", "Stranded", "img/Stranded/Gameplay-V.mp4", 
     `
     <div class="paragraph">
-     <strong>Optimistic Chubby</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
-     <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
+     <strong>Strandede</strong> is a third-person 3D adventure game focused on exploration, survival, and narrative elements.
+     <br> During an international Girl Scout camp at the base of Mount Kinabalu, Kina and her friends are ready for adventure. But when a sudden earthquake strikes during a group hike, chaos erupts—and the girls are separated from the rest of the troop. Lost deep in the jungle, with damaged trails and falling night, they must rely on their scouting skills, bravery, and teamwork to survive. The forest is wild, the clock is ticking, and help may be far away.
+    <br><br>
+    <strong>Can they reunite and escape before darkness closes in?</strong>
     </div>
     <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div class="paragraph center">
-        <a href="https://play.google.com/store/apps/details?id=some.playstore.thing" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
+        <iframe class="youtube" src="https://www.youtube.com/embed/LVDjebhYPi8" frameborder="0" allowfullscreen></iframe>
     </div>
 
     <div class="paragraph">
-        Main features :
+        Main information:
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Adventure</li>
+        <li>Unreal Engine</li>
+        <li>3D</li>
+        <li>Third Person Perspective</li>
+        <li>Story</li>
         </ul>
     </div>
 
     <div class="paragraph center">
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-        <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="Optimistic Chubby Screenshot" />
-    </div>
-    `, "#23bd69", true),
-    new ProjectData("project-2", "Singing Addict", "img/projects/project-2-icon.png", `
-    <div class="paragraph">
-        <strong>Optimistic Chubby Blog</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
-    </div>
-    <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
-
-    <div class="paragraph">
-        Main features :
-        <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-        </ul>
-    </div>
-
-    <div class="paragraph">
-    <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
-    </div>
-    </div>
-
-    <div class="paragraph center">
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Singing Addict Screenshot" />
-    </div>
-    `, "#5a78af"),
-    new ProjectData("project-3", "Drawing Overload", "img/projects/project-3-icon.png", `
-    <div class="paragraph">
-        <strong>Drawing Overload</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.
-    </div>
-    <div class="paragraph center">
-        <iframe class="youtube" src="https://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>
-    </div>
-
-    <div class="paragraph">
-        Main features :
-        <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
-        </ul>
-    </div>
-
-    <div class="paragraph">
-        <div class="notice">
-        Playable in the browser (WebGL) on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code is available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        <img class="pc-screenshot" src="img/Stranded/5.png" /> 
+        <img class="pc-screenshot" src="img/Stranded/1.png" /> 
+        <img class="pc-screenshot" src="img/Stranded/2.png" /> 
+        <img class="pc-screenshot" src="img/Stranded/3.png" /> 
+        <img class="pc-screenshot" src="img/Stranded/4.png" /> 
+        <img class="pc-screenshot" src="img/Stranded/6.png" > 
         </div>
+    `, "#23bd69", true),
+
+    //Manner Dash
+    new ProjectData("project-2", "Manner Dash", "img/MannerDash/5.png", `
+    <div class="paragraph">
+        <strong>Manner Dash</strong> is an endless runner game that progresses through life stages such as elementary school, high school, and university with integrated fight scenes unique to each stage.
+        <br>Ahmad is a boy with a deep love for studying and a strong passion for learning. As he grows up, his educational journey becomes filled with challenges, distractions, and choices that shape his future. Players must help Ahmad stay focused on his dreams while avoiding the negative influences and setbacks that threaten his path. Throughout it all, Ahmad remains kind, respectful, and always ready to help others—proving that true success is measured not only by achievement, but by character. This game aims to raise awareness about the value of education and the real-life struggles many students face.
+        <br><br>
+        <strong>Will Ahmad rise above the noise—or be pulled away from his purpose?</strong>
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/VxQYBBkeiDE" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+        Main information:
+        <ul>
+        <li>Casual Action-Arcade</li>
+        <li>Unity</li>
+        <li>Third Person Perspective</li>
+        </ul>
     </div>
 
     <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drawing Overload Screenshot" />
+    <img class="pc-screenshot" src="img/MannerDash/7.png" />
+    <img class="pc-screenshot" src="img/MannerDash/2.png" />
+    <img class="pc-screenshot" src="img/MannerDash/5.png" />
+    <img class="pc-screenshot" src="img/MannerDash/6.png" />
+    <img class="pc-screenshot" src="img/MannerDash/4.png" >
     </div>
-    `, "#383838"),
-    new ProjectData("project-4", "Eugeneable", "img/projects/project-4-icon.png", `
+    `, "#e48246"),
+
+    //Virus Dash
+    new ProjectData("project-3", "Virus Dash", "img/VirusDash/3.png", `
     <div class="paragraph">
-    <strong>Eugeneable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-    <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@neo8iam">NEOSiAM 2020</a>.
+        <strong>Drawing Overload</strong> is an endless runner game with a Covid-19 virus awareness theme.
+        <br>Aiman, a healthy man in his 20s, finds himself in a dangerous race for survival. As he steps outside, the deadly Covid-19 virus begins to chase him—along with other hazardous viruses lurking in his path. Now, he must run, dodge, and weave through obstacles to stay safe. This fast-paced endless runner challenges players to help Aiman avoid infection and keep moving forward.
+        <br><br>
+        <strong>Can he escape the virus and stay healthy, or will danger catch up to him?</strong>
+    </div>
+
+    <div class="paragraph">
+        Main information:
+        <ul>
+         <li>Casual Action-Arcade</li>
+        <li>Unity</li>
+        <li>Third Person Perspective</li>
+        <li>Windows & Android</li>
+        </ul>
+    </div>>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/VirusDash/1.png" />
+        <img class="pc-screenshot" src="img/VirusDash/2.png" />
+        <img class="pc-screenshot" src="img/VirusDash/3.png" />
+        <img class="pc-screenshot" src="img/VirusDash/4.png" >
+    </div>
+    `, "#F0C75E"),
+
+    //Buds Saga
+    new ProjectData("project-4", "Buds Saga", "img/BudsSaga/1.png", `
+    <div class="paragraph">
+    <strong>Buds Saga</strong>is a turn-based battle game where strategy is key.
+    <br>Get yourself into the heated battle among the AirAsia Buds and win as many battle rounds as possible.Choose your desirable Buds to play with and enjoy their unique perks and statuses. Match against other randomized Buds and battle them out. Choose your upgrades for your player's status depending on your chosen character and get yourselves a rematch against stronger opponents each round.
+    <br><br>
+    <strong>How many rounds can you last?</strong>
     </div>
     
     <div class="paragraph">
-        Main features :
+        More Information:
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Turn-Based Combat</li>
+        <li>Unity</li>
+        <li>Webgl</li>
         </ul>
     </div>
 
     <div class="paragraph">
         <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
+        Windows build available on <a href="https://1im19.itch.io/buds-saga" target="_blank">itch.io</a>.
         </div>
     </div>
 
     <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Eugeneable Screenshot" />
+        <img class="pc-screenshot" src="img/BudsSaga/3.png" />
+        <img class="pc-screenshot" src="img/BudsSaga/1.png" />
+        <img class="pc-screenshot" src="img/BudsSaga/2.png" >
     </div>
-    `, "#e80fb7"),
-    new ProjectData("project-5", "Cloud Drew Land", "img/projects/project-5-icon.png", `
+    `, "#f08080"),
+
+    //Bubbles Up!
+    new ProjectData("project-5", "Bubbles Up!", "img/BubblesUp!/5.png", `
     <div class="paragraph">
-        <strong>Cloud Drew Land</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
+        <strong>Bubbles Up</strong> is a bubble puzzle game with a fun twist!
+        <br/>A young boy playing in the bath discovers he can blow and merge identical bubbles to create bigger ones—just like a bubbly version of Tetris. 
+        <br><br>
+        <strong>Stack, match, and grow your bubbles, but be careful—how big can you go before it all bursts?</strong>
         <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@cottonbro">cottonbro</a>.
     </div>
     
     <div class="paragraph">
-        Main features :
+        Main information:
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Puzzle</li>
+        <li>Unity</li>
+        <li>Webgl</li>
         </ul>
     </div>
 
     <div class="paragraph">
         <div class="notice">
-        Windows build available on <a href="https://some.where/nice" target="_blank">itch.io</a>.
-        Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
+        Windows build available on <a href="https://1im19.itch.io/bubbles-up" target="_blank">itch.io</a>.
         </div>
     </div>
 
     <div class="paragraph center">
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
-        <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Cloud Drew Land Screenshot" />
-    </div>`, "#e48246")
+        <img class="pc-screenshot" src="img/BubblesUp!/1.png" />
+        <img class="pc-screenshot" src="img/BubblesUp!/2.png" />
+        <img class="pc-screenshot" src="img/BubblesUp!/3.png" >
+    </div>`, "#5a78af")
 ];
--- src/data/OtherProjectsData.ts
+++ src/data/OtherProjectsData.ts
@@ -1,106 +1,133 @@
 import ProjectData from '@/data/ProjectData.ts'
 
+
+
 export default [
-    new ProjectData("project-6", "Sultans Of Sing", "img/projects/project-6-icon.png", `
+
+  
+  // Wanita Melayu Terakhir Skit
+    new ProjectData("project-6", "Wanita Melayu Terakhir", "img/WMT/Poster.png", `
     <div class="paragraph">
-      <strong>Sultans Of Sing</strong> is a thing of beauty that I am so proud of. It's available on Windows Store because why not.
-      <br/>Image by <a href="https://www.pexels.com/fr-fr/@aldoalz" target="blank">Aldo Picaso</a>.
-            </div>
-            <div class="paragraph center">
-              <a href="https://www.microsoft.com/en-us/p/some-app" target="_blank"><img src="img/projects/ms-store-logo.png" alt="MS Store button" /></a>
-          </div>
+      <strong>Wanita Melayu Terakhir</strong> is a comedic skit short film created for light-hearted entertainment.
+      <br>A group of boys compliment a girl's dress, but she takes it the wrong way and accuses them of harassment, sparking an argument. The boys, annoyed, fire back by saying, “Being a girl is awesome!”—and that’s when things take a turn. Suddenly, they find themselves in hilarious situations facing “girl problems” like makeup struggles, tudung mishaps, fangirl drama, and the ultimate challenge: dealing with periods.
+      <br><br>
+      <strong>Can they survive a day in a girl’s shoes—or will they tap out too soon?</strong>
+      </div>
+
+      <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/mpPFbrygwKo" frameborder="0" allowfullscreen></iframe>
+      </div>
 
           <div class="paragraph">
-            Main features :
+            Main information:
             <ul>
-            <li>Some stuff</li>
-            <li>Some great stuff</li>
-            <li>More awesome stuff</li>
-            <li>And then some</li>
+            <li>Short film</li>
+            <li>Entertainment</li>
+            <li>Skits</li>
             </ul>
         </div>
 
         <div class="paragraph center">
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" />
-          <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Sultans Of Sing Screenshot" /> 
+          <img class="pc-screenshot" src="img/WMT/1.png" />
+          <img class="pc-screenshot" src="img/WMT/2.png" />
+          <img class="pc-screenshot" src="img/WMT/3.png" />
+          <img class="pc-screenshot" src="img/WMT/4.png" />
+          <img class="pc-screenshot" src="img/WMT/5.png" >
         </div>
-    `, "#c10606", false, true),
-    new ProjectData("project-7", "Drew Blood", "img/projects/project-7-icon.png", `
+    `, "#CBA0E0", false, true),
+
+    // Nature
+    new ProjectData("project-7", "Nature", "img/Nature/3.png", `
     <div class="paragraph">
-                <strong>Drew Blood</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-                <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@karolina-grabowska">Karolina Grabowska</a>.
-            </div>
-
-            <div class="paragraph">
-              Main features :
-              <ul>
-              <li>Some stuff</li>
-              <li>Some great stuff</li>
-              <li>More awesome stuff</li>
-              <li>And then some</li>
-              </ul>
-          </div>
-
-            <div class="paragraph">
-              <div class="notice">
-                You can download and try the app for free or purchase it directly from <a href="http://www.somewbsite.com/" target="_blank">Some website</a>
-              </div>
+                <strong>Nature</strong> is a short-form video that highlights the beauty of the environment, serving as a gentle reminder for people to appreciate and protect the natural world.            
             </div>
 
             <div class="paragraph center">
-                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" />
-                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" />
-                <img class="pc-screenshot" src="https://fakeimg.pl/534x300/" alt="Drew Blood Screenshot" /> 
-            </div>`, "#1ca1e2"),
-    new ProjectData("project-8", "The Art of Walking", "img/projects/project-8-icon.png", `
-    <div class="paragraph">
-                <strong>The Art of Walking</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-            </div>
-
-            <div class="paragraph center">
-              <a href="https://play.google.com/store/apps/details?id=someapp" target="_blank"><img src="img/projects/play-store-logo.png" alt="Play Store badge" /></a>
-          </div>
-
-          <div class="paragraph">
-          Main features :
-          <ul>
-          <li>Some stuff</li>
-          <li>Some great stuff</li>
-          <li>More awesome stuff</li>
-          <li>And then some</li>
-          </ul>
+        <iframe class="youtube" src="https://www.youtube.com/embed/tHQ6V4_Q_io" frameborder="0" allowfullscreen></iframe>
       </div>
 
             <div class="paragraph">
-              <div class="notice">
-                Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
-              </div>
+              Main information:
+              <ul>
+              <li>Short-form video</li>
+              <li>Awareness</li>
+              <li>Love and Care</li>
+              </ul>
+          </div>
+
+            <div class="paragraph center">
+                <img class="pc-screenshot" src="img/Nature/1.png"/>
+                <img class="pc-screenshot" src="img/Nature/2.png"/>
+                <img class="pc-screenshot" src="img/Nature/3.png">
+            </div>`, "#A9D6A4"),
+
+    // Make yourself EPIC MYE
+    new ProjectData("project-8", "Yourself", "img/MYE/2.png", `
+    <div class="paragraph">
+                <strong>Yourself</strong> is a short video project designed to practice various cinematic shooting techniques—including different shot types and camera movements. The goal is to highlight a talent or personal expression while helping you become more comfortable in front of the camera.
+                <br><br>
+                <strong>Make Yourself EPIC</strong>            
+                </div>
+
+            <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/BrqLpRjNpKA" frameborder="0" allowfullscreen></iframe>
+      </div>
+
+          <div class="paragraph">
+          Main information:
+          <ul>
+          <li>Short video</li>
+          <li>Motivation</li>
+          <li>Cinematic</li>
+          </ul>
+      </div>
+
+            <div class="paragraph center">
+                <img class="phone-screenshot" src="img/MYE/1.png" />
+                <img class="phone-screenshot" src="img/MYE/2.png" />
+                <img class="phone-screenshot" src="img/MYE/3.png" />
+                <img class="phone-screenshot" src="img/MYE/4.png" />
+                <img class="phone-screenshot" src="img/MYE/5.png" >
+            </div>`, "#7FBDE8"),
+
+            //Rigibody and Softbody
+    new ProjectData("project-9", "Rigid & Soft", "img/Physics/RS.png", `
+     <div class="paragraph">
+                Simulating a Rigidbody and Softbody Dreaming
             </div>
 
             <div class="paragraph center">
-                <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
-                <img class="phone-screenshot" src="https://fakeimg.pl/300x534/" alt="The Art of Walking Screenshot" />
-            </div>`),
-    new ProjectData("project-9", "Nikolable", "img/projects/project-9-icon.png", `
-     <div class="paragraph">
-                <strong>Nikolable</strong> is a thing of beauty that I am so proud of. I could write about it for hours.
-                <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@goumbik">Lukas</a>.
-            </div>
+        <iframe class="youtube" src="https://www.youtube.com/embed/_nShxUATLRA" frameborder="0" allowfullscreen></iframe>
+      </div>
 
             <div class="paragraph">
-        Main features :
+        Main information:
         <ul>
-        <li>Some stuff</li>
-        <li>Some great stuff</li>
-        <li>More awesome stuff</li>
-        <li>And then some</li>
+        <li>Rigidbody</li>
+        <li>Softbody</li>
+        <li>Unity</li>
+        <li>Physics</li>
         </ul>
     </div>
+            </div>`, "#B3A7E4"),
+
+            //Particles System
+    new ProjectData("project-9", "Particles", "img/Physics/Particles.png", `
+     <div class="paragraph">
+                <strong>Particles System</strong>
+            </div>
+
+            <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube.com/embed/pz9hIqr_D34" frameborder="0" allowfullscreen></iframe>
+      </div>
 
             <div class="paragraph">
-              <div class="notice">
-                Source code available on <a href="https://github.com/yourself" target="_blank">GitHub</a>.
-              </div>
-            </div>`),
+        Main information:
+        <ul>
+        <li>Particles</li>
+        <li>Unity</li>
+        <li>Physics</li>
+        </ul>
+    </div>
+            </div>`, "	#A9A9A9"),
 ];
